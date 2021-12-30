42ef18991d0220af5da7f08c55dd140f1290ffc5
William Sackis Netto <William_Sackis_netto@Dell.com>
2021-12-30 17:32:42 -0300

    Adding more audios and playing icon

--- src/App.js
+++ src/App.js
@@ -1,26 +1,48 @@
-import { useState, useEffect } from 'react'
-import { Button } from 'reactstrap';
+import React, { useState } from 'react'
+import { Button, Row, Col } from 'reactstrap';
 import './App.css';
-import shishoo from './audios/shishoo.m4a'
+import shishoo from './audios/shishoo.m4a';
+import shhShhShh from './audios/shh-shh-shh.m4a';
+import shhhhhhh from './audios/shhhhhhh.m4a';
+import soundIcon from './image/speaker-icon.svg';
 
-export const App = () =>  {
-  let [audio] = useState(new Audio(shishoo));
-  audio.loop = true
+const audios = [
+  {name: 'Shi Shoo', audio: new Audio(shishoo)},
+  {name: 'Shh Shh Shh', audio: new Audio(shhShhShh)},
+  {name: 'Shhhhhh', audio: new Audio(shhhhhhh)}
+];
+
+export const App = () =>  { 
+  const [isPlaying, setIsPlaying] = useState(false);
   
-  const [playing, setPlaying] = useState(false);
+  const getCurrentAudio = () => {
+    return audios.find(obj => false === obj.audio.paused);
+  }
 
-  const toggle = () => setPlaying(!playing);
+  const toggle = (nextAudio) => {
+    const audioObj = getCurrentAudio();
 
-  useEffect(() => {
-      playing ? audio.play() : audio.pause();
-    },
-    [audio, playing]
-  );
+    if (audioObj && audioObj.audio !== nextAudio) {
+        audioObj.audio.pause();
+    }
+
+    nextAudio.loop = true;
+    nextAudio.paused ? nextAudio.play() : nextAudio.pause();
+    setIsPlaying(!isPlaying)
+  }
 
   return (
     <div className="App">
-      <Button className='mt-5' color='primary' onClick={toggle}>Shi Shoo</Button>
-
+      <Row className='mt-5'>
+      { audios.map((item) => 
+        <Col>
+          <Button color='success' size='lg' onClick={() => toggle(item.audio)} style={{width: '200px'}}>
+            {!item.audio.paused && <img src={soundIcon} alt='play' height="32" width="32" style={{marginRight: '10px'}} />}
+            {item.name}
+          </Button>
+        </Col>
+      )}
+      </Row>
     </div>
   );
 }
